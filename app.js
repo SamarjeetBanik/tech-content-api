7ef898dea6a774a4c12207826095638ce0e4c3ef
SamarjeetBanik <samarjeetbanik@gmail.com>
2021-06-01 00:44:01 +0530

    added db connection code

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 require('dotenv').config();
+
 const express = require('express')
 const app = express()
-const db = require('./db');
+
+const mongoose = require('mongoose');
 
 const ContentController = require('./ContentController');
 app.use('/', ContentController);
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => {
+    console.log("DB CONNECTED");
+}).catch((err) => {
+    console.log(err);
+})
 
 module.exports = app
